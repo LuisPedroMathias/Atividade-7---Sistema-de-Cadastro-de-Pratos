ad7d502234a627bfbf90b2377bdc1ec41f6c30e4
Lazzarott1 <lucas_lazzarotti@estudante.sesisenai.org.br>
2026-08-19 11:03:56 -0300

    alt - correção ddl

--- Banco de Dados/database.sql
+++ Banco de Dados/database.sql
@@ -2,16 +2,16 @@ CREATE DATABASE sistema_pratos_LandP;
 USE sistema_pratos_LandP;
 
 CREATE TABLE usuarios (
-    nome VARCHAR(200) AUTO_INCREMENT PRIMARY KEY,
+    nome_user VARCHAR(200) PRIMARY KEY,
     email VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE pratos (
     idprato INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(200) NOT NULL,
+    nome_prato VARCHAR(200) NOT NULL,
     descricao LONGTEXT NOT NULL,
     preco DECIMAL(10, 2) NOT NULL,
     categoria VARCHAR(100) NOT NULL,
     nome_user VARCHAR(200) NOT NULL,
-    FOREIGN KEY (nome_user) REFERENCES usuarios(nome)
+    FOREIGN KEY (nome_user) REFERENCES usuarios(nome_user)
 );
