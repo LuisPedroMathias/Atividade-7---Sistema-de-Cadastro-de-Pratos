8f62b2fcbebde065f561bcf71086cef2d7b2e83f
Lazzarott1 <lucas_lazzarotti@estudante.sesisenai.org.br>
2026-08-20 07:38:20 -0300

    alt - matando o leao

--- Public/editar.php
+++ Public/editar.php
@@ -15,12 +15,18 @@ if (!$prato) {
     die('Prato não encontrado.');
 }
 
+$usuarios = mysqli_query($conexao, "SELECT nome_user FROM usuarios");
+
+if (!$usuarios) {
+    die("Erro na consulta: " . mysqli_error($conexao));
+}
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $nome_prato = trim($_POST['nome_prato']);
     $descricao = trim($_POST['descricao']);
     $preco = $_POST['preco'];
     $categoria = trim($_POST['categoria']);
-    $usuario_id = (int) $_POST['usuario'];
+    $nome_user = trim($_POST['nome_user']);
 
     $sql = "UPDATE pratos SET nome_prato = ?, descricao = ?, preco = ?, categoria = ?, nome_user = ? WHERE idprato = ?";
     $stmt = mysqli_prepare($conexao, $sql);
@@ -43,7 +49,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Editar Prato</title>
+    <h1>Editar Prato!</h1>
     <link rel="stylesheet" href="../styles/style.css">
 </head>
 
@@ -52,18 +58,23 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         <label for="nome">Nome:</label>
         <input type="text" name="nome_prato" id="nome_prato" value="<?php echo htmlspecialchars($prato['nome_prato']); ?>" required>
+        <br>
         <label for="descricao">Descrição:</label>
         <input type="text" name="descricao" id="descricao" value="<?php echo htmlspecialchars($prato['descricao']); ?>" required>
-        <label for="preco">Preço:</label>
-        <input type="number" name="preco" id="preco" value="<?php echo htmlspecialchars($prato['preco']); ?>" step="0.01" required>
+        <br>
         <label for="categoria">Categoria:</label>
         <input type="text" name="categoria" id="categoria" value="<?php echo htmlspecialchars($prato['categoria']); ?>" required>
+        <br>
+        <label for="preco">Preço:</label>
+        <input type="number" name="preco" id="preco" value="<?php echo htmlspecialchars($prato['preco']); ?>" step="0.01" required>
+        <br>
         <label for="usuario">Usuário:</label>
             <select name="nome_user" id="nome_user">
                 <?php while ($usuario = mysqli_fetch_assoc($usuarios)) { ?>
                     <option value="<?php echo $usuario["nome_user"] ?>"><?php echo $usuario["nome_user"] ?></option>
                 <?php } ?>
             </select>
+        <br>
         <button type="submit">Atualizar Prato</button>
     </form>
     <button type="button" onclick="window.location.href='../index.php'">Voltar</button>
